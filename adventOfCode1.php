b2088464479885c62634db3e5bef754d20e01a93
Thierry CORRE <corre.t@gmail.com>
2015-12-04 12:14:52 +0100

    Day 1

---
+++ adventOfCode1.php
@@ -0,0 +1,20 @@
+<?php
+$string = file_get_contents('adventOfCode1_input.txt');
+
+$cpt = 0;
+$strLen = strlen($string);
+$isOk = true;
+for ($i = 0; $i < $strLen; $i++) {
+    if ($string[$i] == '(') {
+        $cpt++;
+    } else if ($string[$i] == ')') {
+        $cpt--;
+    }
+    if ($cpt < 0 && $isOk === true) {
+        print 'First underground: '.($i+1);
+        print PHP_EOL;
+        $isOk = false;
+    }
+}
+print 'Result: '.$cpt;
+print PHP_EOL;
