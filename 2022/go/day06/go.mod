5b09eb1932d78150148bfccbcac87b206c862aae
redboool <corre.t@gmail.com>
2022-12-06 09:16:08 +0100

    Day 06 of 2022

---
+++ 2022/go/day06/go.mod
@@ -0,0 +1,3 @@
+module adventofcode/day06
+
+go 1.19
---
+++ 2022/go/day06/part2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	filePath := os.Args[1]
+	readFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer readFile.Close()
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	j := 0
+	for fileScanner.Scan() {
+		strVal := fileScanner.Text()
+
+		packet := make([]uint8, 14)
+		for j = 0; j < len(strVal); j++ {
+			packet[j%14] = strVal[j]
+			if j < 14 {
+				continue
+			}
+
+			uniqSlice := unique2(packet)
+
+			if len(uniqSlice) == 14 {
+				break
+			}
+		}
+		break
+	}
+
+	fmt.Println("Result: ", j+1)
+}
+
+func unique2(input []uint8) []uint8 {
+	u := make([]uint8, 0, len(input))
+	m := make(map[uint8]bool)
+
+	for _, val := range input {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+
+	return u
+}
